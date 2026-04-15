7feba45792e5f982dfcd734221400b3b79fb1117
jz7911 <joe@bgparks.org>
2026-04-14 21:10:03 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ const PROGRAM_TYPES = [
 const ADMIN_OVERHEAD_RATE  = 0.1;
 const FT_ANNUAL_SALARY     = 97700;
 const FACILITY_COST_PER_HR = 3;
-const MANAGER_NAMES        = ["admin","manager","joe zimmermann","erika strojinc","dan stanczak","brian o'malley","chris eckert","chuck burgess","diana clayson","amanda busch","laurie hoffman"];
+const MANAGER_NAMES        = ["admin","manager","joe zimmermann","erika strojinc","dan stanczak","brian o'malley","chris eckert","chuck burgess","diana clayson","amanda busch","laurie hoffman","Mike Gianatasio"];
 
 
 // Service category cost recovery targets
